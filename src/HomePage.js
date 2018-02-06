12669b610cd7b3af46bb48971d06b244d2610584
afreen24 <studentinsha12@gmail.com>
2018-02-06 20:28:13 +0530

    added routing

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,32 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import ToolBarTop from './Components/ToolBarTop';
-import About from './Components/About';
-import P3 from './Components/P3';
-import PrivacyPolicy2 from './Components/PrivacyPolicy2';
-import ProfileC from './Components/ProfileC'
+import Home from './Components/home';
+import Payment from './Payment';
+import { Switch , Route} from 'react-router-dom';
 
-const gridContainer = {
-  display: 'grid',
-  gridTemplateColumns: '29% 45% 33%',
-}
-
-const HomePage = () => (
+const HomePage = (props) => (
   <MuiThemeProvider>
-    <ToolBarTop />
-    <div style={gridContainer}>
-      <div class="class-items">
-        <ProfileC />
-        <About />
-      </div>
-      <div class="class-items">
-        <P3 />
-      </div>
-      <div class="class-items">
-        <PrivacyPolicy2 />
-      </div>
-    </div>
+    <Switch>
+      <Route exact path = "/" component={Home} />
+      <Route path="/payment" component ={Payment}/>
+    </Switch>
   </MuiThemeProvider>
 );
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Payment from './Payment';
-import HomePage from './HomePage'
+import HomePage from './HomePage';
+import { BrowserRouter} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<HomePage />, document.getElementById('root'));
+ReactDOM.render(
+	<BrowserRouter>
+	  <HomePage />
+	</BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
